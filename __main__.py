f9eb1216bd535652f54ad9cdf344991c5e3e536b
Aditya Prakash <prakash.adi@northeastern.edu>
2023-10-25 19:30:24 -0400

    Update __main__.py
    changes related to AWS RDS

--- __main__.py
+++ __main__.py
@@ -2,17 +2,18 @@ import boto3
 import os
 import pulumi_aws as aws
 import pulumi
+from pulumi_aws import rds
 from dotenv import load_dotenv
 from pulumi_aws import get_availability_zones
 
 load_dotenv()
 
-aws_profile = "demo" 
+# aws_profile = "dev" 
 
 config = pulumi.Config("aws")
 region = config.require("region")
 
-session = boto3.Session(profile_name=aws_profile, region_name=region)
+session = boto3.Session(profile_name=config.get("profile"), region_name=region)
 
 def create_vpc(ec2):
     vpc = ec2.create_vpc(CidrBlock=os.getenv("vpc_cidr_block"))
@@ -69,6 +70,47 @@ def create_or_get_key_pair(ec2_client):
     # Use the public key from the new key pair with Pulumi's aws.ec2.KeyPair
     pulumi_key_pair = aws.ec2.KeyPair("my-keypair_demo", public_key=new_key_pair["KeyMaterial"])
     return pulumi_key_pair.key_name
+
+def create_rds_parameter_group():
+    parameter_group = rds.ParameterGroup("webapp-db-parameter-group",
+        family="mysql8.0",
+        description="Custom DB Parameter Group",
+        parameters=[{
+            "name": "character_set_server",
+            "value": "utf8mb4",
+        },
+        {
+            "name": "collation_server",
+            "value": "utf8mb4_unicode_ci",
+        },
+        {
+            "name": "max_connections",
+            "value": "100", 
+            "apply_method": "pending-reboot"
+         }])
+
+    return parameter_group
+
+def create_rds_instance(db_security_group, db_parameter_group, private_subnets):
+    rds_instance = rds.Instance("webapp-db",
+        allocated_storage=20,
+        storage_type="gp2",
+        engine="mysql",
+        engine_version="8.0",
+        instance_class="db.t2.micro",  
+        db_name="csye6225",
+        parameter_group_name=db_parameter_group.name,
+        password="Cloud2023",
+        username="csye6225",
+        publicly_accessible=False,
+        skip_final_snapshot=True,
+        vpc_security_group_ids=[db_security_group.id],
+        db_subnet_group_name=rds.SubnetGroup("webapp-db-subnet-group",
+            subnet_ids=[subnet.id for subnet in private_subnets]
+        ).name,
+    )
+
+    return rds_instance
 
 
 def fetch_ami_id(ec2_client):
@@ -177,10 +219,92 @@ def main():
             type="ingress",
         )
 
+    egress_rules = [
+        aws.ec2.SecurityGroupIngressArgs(
+            description="Allow outbound traffic from the application",
+            from_port=0,
+            to_port=65535,
+            protocol="tcp",
+            cidr_blocks=[os.getenv("ig_cidr_block")]
+        ),
+    ]
+    
+    # Apply the egress rules to the security group
+    for rule in egress_rules:
+        aws.ec2.SecurityGroupRule(
+            f"egress-rule-{rule.from_port}-{rule.to_port}",
+            security_group_id=application_security_group.id,
+            from_port=rule.from_port,
+            to_port=rule.to_port,
+            protocol=rule.protocol,
+            cidr_blocks=rule.cidr_blocks,
+            description=rule.description,
+            type="egress",
+        )    
+       
+
     print(fetch_ami_id(ec2_client))
     
     key_name = create_or_get_key_pair(ec2_client)
     
+    # Creating Database Security Group
+    db_security_group = aws.ec2.SecurityGroup(
+        "db-security-group",
+        description="Security Group for RDS instances",
+        vpc_id=vpc.id,
+    )
+    
+    db_ingress_rules = [
+        aws.ec2.SecurityGroupIngressArgs(
+            description="Allow MySQL traffic from application",
+            from_port=3306,
+            to_port=3306,
+            protocol="tcp",
+        ),
+    ]
+
+    db_egress_rules = [
+        aws.ec2.SecurityGroupIngressArgs(
+            description="Allow outbound traffic from the application",
+            from_port=0,
+            to_port=65535,
+            protocol="tcp",
+        ),
+    ]
+
+    # Adding ingress rule for MySQL
+    for db_rule in db_ingress_rules:
+        aws.ec2.SecurityGroupRule(
+            f"db-ingress-rule-{db_rule.from_port}-{db_rule.to_port}",
+            security_group_id=db_security_group.id,
+            source_security_group_id=application_security_group.id,
+            from_port=db_rule.from_port,
+            to_port=db_rule.to_port,
+            protocol=db_rule.protocol,
+            description=db_rule.description,
+            type="ingress",
+        )
+
+    # Adding ingress rule for MySQL
+    for db_rule in db_egress_rules:
+        aws.ec2.SecurityGroupRule(
+            f"db-egress-rule-{db_rule.from_port}-{db_rule.to_port}",
+            security_group_id=db_security_group.id,
+            source_security_group_id=application_security_group.id,
+            from_port=db_rule.from_port,
+            to_port=db_rule.to_port,
+            protocol=db_rule.protocol,
+            description=db_rule.description,
+            type="egress",
+        )
+        
+        
+    db_parameter_group = create_rds_parameter_group()
+    rds_instance = create_rds_instance(db_security_group, db_parameter_group, private_subnets)
+    
+    print(rds_instance)
+
+
     ec2_instance = aws.ec2.Instance (
         "webapp-ec2-instance",
         instance_type = "t2.micro",
@@ -189,11 +313,26 @@ def main():
         associate_public_ip_address = True,
         ami = fetch_ami_id(ec2_client),  # Use the AMI ID from Packer
         key_name=key_name,
+        user_data=pulumi.Output.all(rds_instance.endpoint, rds_instance.username, rds_instance.password).apply(
+                    lambda args: f"""#!/bin/bash
+                    export DB_HOST={args[0]}
+                    export DB_NAME={args[1]}
+                    export DB_USER={args[1]}
+                    export DB_PASSWORD={args[2]}
+                    """
+                    ),
         tags = {
             "Name": "WebApp_EC2Instance",
         },
-    )
+        )
 
 
+    # Export the RDS endpoint
+    pulumi.export("db_endpoint", rds_instance.endpoint)
+
+    # Export the EC2 instance's public IP
+    pulumi.export("ec2_public_ip", ec2_instance.public_ip)    
+
+    
 if __name__ == '__main__':
     main()
